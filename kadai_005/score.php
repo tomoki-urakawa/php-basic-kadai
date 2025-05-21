b0df54054dc4d1131afde39ad2852d0a0ab4a47f
tomoki-urakawa <winas.tomoki.urakawa@gmail.com>
2025-05-21 16:12:57 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>この文章はHTMLで出力しています。</p>
+   <p>
+       <?php
+      $score1 = 80;
+      $score2 = 60;
+      $score3 = 55;
+      $score4 = 40;
+      $score5 = 100;
+      $score6 = 25;
+      $score7 = 80;
+      $score8 = 95;
+      $score9 = 30;
+      $score10 = 60;
+
+      // 上の10人分の点数を足し算
+      $total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+      // 平均点を計算する（合計を10で割る）
+      $average = $total / 10;
+
+      // echoでブラウザに出力
+      echo "平均点は " . $average . " 点です。";
+       ?>
+   </p>
+</body>
+
+</html>
